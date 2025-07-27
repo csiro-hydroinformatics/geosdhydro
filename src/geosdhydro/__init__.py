9740025ede51c684d362c5d0db03c81202714d14
J-M <jean-michel.perraud@csiro.au>
2025-07-27 21:04:38 +1000

    ci: adjust so that check-docs passes

--- src/geosdhydro/__init__.py
+++ src/geosdhydro/__init__.py
@@ -7,4 +7,4 @@ from __future__ import annotations
 
 from geosdhydro._internal.cli import get_parser, main
 
-__all__: list[str] = ["get_parser", "main"]
+__all__: list[str] = ["get_parser", "main", "swift"]
